1698d5b752dabeab22b74c0420e0e3f262693cf5
Mitushi-23 <mitushi2agrawal@gmail.com>
2024-07-05 16:18:15 +0530

    CRUD operations in Employee-SpringBoot-JPA

---
+++ Employee-SpringBoot-JPA/src/main/java/com/employeeJPA/employee/EmployeeService.java
@@ -0,0 +1,36 @@
+package com.employeeJPA.employee;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Optional<Employee> getEmployeeById(int id) {
+        return employeeRepository.findById(id);
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(int id) {
+        employeeRepository.deleteById(id);
+    }
+}
+
